639d32c03ed8b0e75437f555390dc55f042def72
manishjoy <mjoy@excellencetechnologies.in>
2016-02-23 18:35:17 +0530

    code

--- module/app/code/local/Excellence/Test/controllers/IndexController.php
+++ module/app/code/local/Excellence/Test/controllers/IndexController.php
@@ -6,42 +6,39 @@ class Excellence_Test_IndexController extends Mage_Core_Controller_Front_Action
 		$this->loadLayout();     
 		$this->renderLayout();
     }
-    public function deleteAction(){
-    	$this->loadLayout();     
-		$this->renderLayout();
 
+    public function deleteAction()
+    {
 		$id = Mage::app()->getRequest()->getParam("id");
 		$module_name = Mage::app()->getRequest()->getParam("module_name");
-		//Mage::getModel('test/'.$module_name)->deleteRow($id);
 		if(Mage::getModel('test/'.$module_name)->deleteRow($id)){
-			Mage::getSingleton('core/session')->addSuccess("Row Deleted");
-			session_write_close(); 
+			Mage::getSingleton('core/session')->addSuccess(Mage::helper('test')->__('Row Deleted'));
 			$this->_redirect('test/index/index');
 		}
 		else{
-			Mage::getSingleton('core/session')->addError("Some Error Occured.... Please try again...");
-			session_write_close();
+			Mage::getSingleton('core/session')->addError(Mage::helper('test')->__('Some Error Occured.... Please try again...'));
 		}
-    }
-    public function addAction(){
-    	$this->loadLayout();     
+		$this->loadLayout();     
 		$this->renderLayout();
+    }
+
+    public function addAction()
+    {
 		$module_name = Mage::app()->getRequest()->getParam("module_name");
 		$post = Mage::app()->getRequest()->getParams();
 		if(isset($post['sub']) && !empty($post['title']) && !empty($post['content'])){
 			if(Mage::getModel('test/'.$module_name)->saveRow($post)){
-				Mage::getSingleton('core/session')->addSuccess("Row Inserted");
-				session_write_close(); 
+				Mage::getSingleton('core/session')->addSuccess(Mage::helper('test')->__('Row Inserted'));
 				$this->_redirect('test/index/index');
 			}
 			else{
-				Mage::getSingleton('core/session')->addError("Some Error Occured.... Please try again...");
-				session_write_close();
+				Mage::getSingleton('core/session')->addError(Mage::helper('test')->__('Some Error Occured.... Please try again...'));
 			}
 		}
 		else{
-			Mage::getSingleton('core/session')->addNotice("Please Fill all the fields correctly...");
-			session_write_close();
+			Mage::getSingleton('core/session')->addNotice(Mage::helper('test')->__('Please Fill All The Fields Correctly'));
 		}
+    	$this->loadLayout();     
+		$this->renderLayout();
     }
 }
